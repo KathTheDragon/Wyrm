1149ee03d152be940171f7962fe766f0f116f2c9
KathTheDragon <kaththedragon@gmail.com>
2019-05-28 14:04:26 +0100

    Add Expression base class

--- wyrm/expression.py
+++ wyrm/expression.py
@@ -1,9 +1,16 @@
 # Temp
-class String:
-    def __init__(string):
+class Expression:
+    def __init__(self):
+        pass
+
+    def evaluate(self, *contexts):
+        return
+
+class String(Expression):
+    def __init__(self, string):
         self.string = string
 
-    def format(self, *contexts):
+    def evaluate(self, *contexts):
         return self.string
 
 def evaluate(string, *contexts):
