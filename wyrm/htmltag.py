e2b38eb9f166b5797301c297d2ecc7280300a582
KathTheDragon <kaththedragon@gmail.com>
2019-05-29 15:27:02 +0100

    Implement functions in htmltag.py

--- wyrm/htmltag.py
+++ wyrm/htmltag.py
@@ -1,3 +1,5 @@
+from expression import Expression
+
 ## Constants
 DOCTYPES = {
     '5': '<!doctype html>',
@@ -9,13 +11,88 @@ DOCTYPES = {
     '1 frameset': '<!doctype html PUBLIC "-//W3C//DTD XHTML 1.0 Frameset//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-frameset.dtd">',
     '1.1': '<!doctype html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">',
 }
+SELF_CLOSING = [
+    'area',
+    'base',
+    'br',
+    'col',
+    'command',
+    'embed',
+    'keygen',
+    'hr',
+    'img',
+    'input',
+    'link',
+    'meta',
+    'param',
+    'source',
+    'track',
+    'wbr'
+]
 
 ## Functions
 def make(line):
-    return '', {}
+    # Get tag name
+    _line = line  # Just in case we need to undo
+    name, line = popHTMLIdentifier(line)
+    if line[0].value == '=':
+        name, line = '', _line
+    if not name:
+        name = 'div'
+    # Get id/class shortcuts
+    id = ''
+    classes = []
+    while line[0].value in ('#', '.'):
+        type = line[0].value
+        value, line = popHTMLIdentifier(line[1:])
+        if type == '#':
+            id = value
+        else:
+            classes.append(value)
+    # Get attributes
+    attributes = makeAttributes(line)
+    if id:
+        attributes['id'] = id
+    if classes:
+        if 'class' in attributes:
+            attributes['class'] = ' '.join([attributes['class']] + classes)
+        else:
+            attributes['class'] = ' '.join(classes)
+    return name, attributes
 
 def makeAttributes(line):
-    return {}
+    attributes = {}
+    while line:
+        attr, line = popHTMLIdentifier(line)
+        if line[0].value == '=':
+            expr, line = Expression.pop(line)
+        else:
+            expr = Expression(True)
+        attributes[attr] = expr
+    return attributes
+
+def popHTMLIdentifier(line):
+    if line[0].type != 'IDENTIFIER':
+        return '', line
+    for i, token in enumerate(line):
+        if token.type == 'IDENTIFIER' and line[i-1].value == '-':
+            continue
+        elif token.value == '-' and line[i-1].type == 'IDENTIFIER':
+            continue
+        break
+    else:
+        return ''.join(token.value for token in line), []
+    return ''.join(token.value for token in line[:i]), line[i:]
 
 def render(name, attributes, *contexts):
-    return '', ''
+    for attr, expr in attributes.items():
+        attributes[attr] = expr.evaluate(*contexts)
+    attrList = [(f'{attr}={value!r}' if value is not True else attr) for attr, value in attributes.items() if value]
+    if attrList:
+        open = f'{name} {" ".join(attrList)}'
+    else:
+        open = name
+    if name in SELF_CLOSING:  # This may be a config option
+        return f'<{open} />', None
+    else:
+        return f'<{open}>', f'</{name}>'
